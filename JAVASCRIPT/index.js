88e6c4c0c888aa658d1a86d4e8f180aaea614052
diyasharma19 <diyasharma1905@gmail.com>
2023-10-31 11:47:38 +0530

    Final calculator

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,4 +1,24 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.querySelector("input[name='display']");
+    const buttons = document.querySelectorAll("input[type='button']");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const value = this.value;
+
+            if (value === "AC") {
+                display.value = "";
+            } else if (value === "DE") {
+                display.value = display.value.toString().slice(0, -1);
+            } else if (value === "=") {
+                display.value = eval(display.value);
+            } else {
+                display.value += value;
+            }
+        });
+    });
+});
 
            
 
